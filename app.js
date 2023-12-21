b931eb00c3ecbd19df5ccef0cc9d7f9c1e200014
assafassa <153225733+assafassa@users.noreply.github.com>
2023-12-21 20:00:52 +0200

    mabyr

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ const WebSocket = require('ws')
 const PORT = process.env.PORT || 8080;
 const wss = new WebSocket.Server({ port: PORT })
 const onlinePlayers = new Map();
-const handleConnection = require('./controllers/handleConnection.js');
+
 
 
 wss.on('connection', (ws,request) => {
-    handleConnection(onlinePlayers,ws,request)
+    const clientId = new URL(request.url, `http://${request.headers.host}`).searchParams.get('clientId');
+    const clientName= new URL(request.url, `http://${request.headers.host}`).searchParams.get('username');
+    const unoccupiedPlayers = Array.from(onlinePlayers.entries()).filter(([clientId, player]) => !player.occupied);
+    let dataToSend={}
+    if (unoccupiedPlayers[0]&&unoccupiedPlayers[0]!=clientId){
+        dataToSend.message=unoccupiedPlayers[0][1].username
+        let opponetname=unoccupiedPlayers[0][1].username
+        let ooponws=unoccupiedPlayers[0][1].ws
+        let opponid=unoccupiedPlayers[0][0]
+        onlinePlayers.set(opponid,{ws:ooponws,username:opponetname,occupied:true,opponent:clientId})
+        onlinePlayers.set(clientId, {ws: ws, username: clientName, occupied: true, opponent: opponid})
+        ooponws.send(JSON.stringify({message:clientName,subject:'startnewgame'}))
+
+    }else{
+        dataToSend.message='noavailableplayes'
+        onlinePlayers.set(clientId, {ws: ws, username: clientName, occupied: false, opponent: '' })
+    }
+    ws.send(JSON.stringify(dataToSend))
   ws.on('message', (message) => {
     messagejson=JSON.parse(message.toString('utf-8'))
     let myPlayer = Array.from(onlinePlayers.entries()).filter(([clientId, player]) => clientId==messagejson.clientId);
